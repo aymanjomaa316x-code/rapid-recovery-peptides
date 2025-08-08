baa3e1d68b54c6731dfc920aad0014e7e6cdf7e8
aymanjomaa316x-code <aymanjomaa316x@gmail.com>
2025-08-08 15:12:47 +1000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,16 @@ module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
-    "./app/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {
       colors: {
         blackCustom: "#0B0B0B",
         goldLight: "#D8B85A",
-        goldDark: "#B8902E",
-      },
-    },
+        goldDark: "#B8902E"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
